1f0bbf9bb75c9961117503862dd89ccb0f531b92
Si0dot7 <felm27010@gmail.com>
2025-04-22 00:23:26 +0700

    connect back to front

--- full-stack-nest-react/src/courses.controller.ts
+++ full-stack-nest-react/src/courses.controller.ts
@@ -4,8 +4,22 @@ import { Controller , Get } from "@nestjs/common";
 export class CoursesController {
     @Get()
     findAll(): any {
-        return {
-            message: 'hello'
-        }
+        return [
+            {
+                id:'1',
+                number: '121',
+                title: 'computer'
+            },
+            {
+                id:'2',
+                number: '1121',
+                title: 'discrete'
+            },
+            {
+                id:'3',
+                number: '15121',
+                title: 'math'
+            }
+        ]
     }
 }
