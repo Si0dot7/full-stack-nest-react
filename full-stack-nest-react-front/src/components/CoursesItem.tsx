62623534eefe5a31f5a03bf965bc680c400feca2
Si0dot7 <felm27010@gmail.com>
2025-05-21 11:41:43 +0700

    create form at frontend and fetch to back

--- full-stack-nest-react-front/src/components/CoursesItem.tsx
+++ full-stack-nest-react-front/src/components/CoursesItem.tsx
@@ -11,44 +11,87 @@ const CoursesItem = (props: CoursesItemProps) => {
   const [reviewToggleVisible, setReviewToggleVisible] = useState<boolean>(false)
   const [review, setReview] = useState<Review[]>([]);
 
+  const [newReviewComment, setNewReviewComment] = useState<string>('')
+  const [newReviewScore, setNewReviewScore] = useState<number>(1)
+
+  const newReviewScoreOptions = [1, 2, 3, 4, 5]
+
+
+  const fetchUpdate = () => {
+    if (courses.id) {
+      CoursesService.fetchReview(courses.id)
+        .then(review => {
+          setReview(review)
+        })
+    }
+  }
+
+  const clearNewReviewForm=()=>{
+    setNewReviewComment('')
+    setNewReviewScore(1)
+  }
+
   const toggleVisible = () => {
     if (!reviewToggleVisible) {
-      
-      if (courses.id) {
-        CoursesService.fetchReview(courses.id)
-          .then(review => {
-            setReview(review)
-            setReviewToggleVisible(true)
-            console.log(review);
-            
-          })
-
-      } else {
-        setReviewToggleVisible(true)
-        return <p>don't mate any courseId</p>
-        
-      }
+      fetchUpdate();
+      setReviewToggleVisible(true)
     } else {
       setReviewToggleVisible(false)
     }
   }
 
+  const handleSaveReview = () => {
+    const newReview: Review = {
+      comment: newReviewComment,
+      score: newReviewScore,
+    }
+    if (courses.id) {
+      CoursesService.saveReview(newReview, courses.id)
+        .then(saveNewReview => {
+          if (saveNewReview) {
+            fetchUpdate();
+            clearNewReviewForm();
+          }
+        })
+    }
+  }
+
   return (
-    <div className=" mb-4">
-      <div className="text-red-700">
-        {courses.id},{courses.number},{courses.title}
-      </div>
-      <div>
-        <button className="border bg-blue-300" onClick={toggleVisible}>toggle</button>
-      </div>
-      <div>
-        {reviewToggleVisible &&
-          <div>
-            {review.map(item=>{
-              return <p key={item.id}>{item.comment} | {item.score}</p>
-            })}
-          </div>
-        }
+    <div className=" mb-4 flex justify-center">
+      <div className="block">
+        <div className="text-red-700">
+          {courses.id},{courses.number},{courses.title}
+        </div>
+        <div>
+          <button className="border bg-blue-300" onClick={toggleVisible}>toggle</button>
+        </div>
+        <div>
+          {reviewToggleVisible &&
+            <div>
+              {review.map((item) => {
+                return <p key={item.id}>{item.comment} | {item.score}</p>
+              })}
+            </div>
+          }
+        </div>
+        <div>
+          <section>
+            <h1>Add Request</h1>
+          </section>
+          <section className="flex">
+            <h1>comment: </h1>
+            <input className="border" value={newReviewComment} onChange={(e) => { setNewReviewComment(e.target.value) }} />
+          </section>
+          <section className="flex">
+            <h1>score: </h1>
+            <select className="border" value={newReviewScore} onChange={(e) => { setNewReviewScore(parseInt(e.target.value, 10)) }}>
+              {newReviewScoreOptions.map((item,index) => {
+                return <option key={index} value={item} className="border">{item}</option>
+              })}
+            </select>
+          </section>
+          <button className="border" onClick={handleSaveReview}>Save</button>
+        </div>
       </div>
     </div>
   )
--- full-stack-nest-react-front/src/services/CoursesService.ts
+++ full-stack-nest-react-front/src/services/CoursesService.ts
@@ -27,9 +27,23 @@ async function fetchReview(courseId: string): Promise<Review[]>{
     return review
 }
 
+async function saveReview(newReview: Review , courseId: string): Promise<Review | null>{
+    const res = await fetch(`${import.meta.env.VITE_API_URL}/courses/${courseId}/review`, {
+        method: 'POST',
+        headers: { 'Content-Type': 'Application/json' },
+        body: JSON.stringify(newReview)
+    })
+    const saveNewReview: (Review) = await res.json()
+    if(saveNewReview.id !== undefined){
+        return saveNewReview
+    }else{
+        throw null
+    }
+}
 
 export default {
     fetchCourses,
     createCourses,
     fetchReview,
+    saveReview
 }
